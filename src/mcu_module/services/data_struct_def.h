1561022f4c68c945f7e4e895ec5677f5188cfbcd
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2021-01-09 21:51:20 +0200

    Implement add GPS data to queue

--- src/mcu_module/services/data_struct_def.h
+++ src/mcu_module/services/data_struct_def.h
@@ -60,4 +60,14 @@ typedef struct __attribute__((__packed__)) {
 } GPS_Data_t;
 
 
+typedef struct {
+    uint8_t sync;
+    uint8_t type;
+    uint8_t msg_id;
+    uint8_t len;
+    uint32_t *payload;
+    uint8_t crc8;
+} Send_Message_t;
+
+
 #endif //TELEMETRY_SYSTEM_DATA_STRUCT_DEF_H
